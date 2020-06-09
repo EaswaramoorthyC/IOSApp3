573a95af205e459549d32478906f754f7b381620
Chinnasamy <Easwaramoorthy.Chinnasamy.Contractor@pepsico.com>
2020-06-09 06:47:31 -0500

    Add project files.

---
+++ IOSXamainUITest1/AppInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace IOSXamainUITest1
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+
+
+            return ConfigureApp.iOS.InstalledApp("com.PepsiCo.SalesHub2 - B.QA").StartApp();
+        }
+    }
+}
